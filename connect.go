42785133e9e3e8e2e63c75947ebbd8302af2345d
heimonsy <heimonsy@gmail.com>
2019-10-29 14:48:05 +0800

    feat: impl service handle connection

--- connect.go
+++ connect.go
@@ -1,13 +1,72 @@
 package fxxk
 
+import (
+	"bytes"
+	fmt "fmt"
+	"io"
+)
+
 type connectClient struct {
-	id        string
-	done      chan struct{}
-	stream    Fxxk_ConnectServer
-	idleTunel []tunelStream
+	id          string
+	done        chan struct{}
+	stream      Fxxk_ConnectServer
+	idleTunnels []*tunnelStream
 }
 
-type tunelStream struct {
+type tunnelStream struct {
 	Fxxk_ProxyTunelServer
 	done chan struct{}
+}
+
+func (ts *tunnelStream) Close() {
+	// TODO
+}
+
+func (ts *tunnelStream) ReadWriter() io.ReadWriter {
+	return &wrapTunnelStream{
+		Fxxk_ProxyTunelServer: ts.Fxxk_ProxyTunelServer,
+		buf:                   bytes.NewBuffer(make([]byte, 0, 1024)),
+	}
+}
+
+type wrapTunnelStream struct {
+	Fxxk_ProxyTunelServer
+	buf *bytes.Buffer
+}
+
+func (w *wrapTunnelStream) Read(p []byte) (n int, err error) {
+	if w.buf.Len() > 0 {
+		return w.buf.Read(p)
+	}
+
+	req, err := w.Recv()
+	if err != nil {
+		return
+	}
+	var data []byte
+	{
+		v, ok := req.Req.(*TunnelRequest_Data)
+		if !ok {
+			return 0, fmt.Errorf("invalid client request: expect data")
+		}
+		data = v.Data.Data
+	}
+
+	n = copy(p, data)
+	if len(data) > n {
+		w.buf.Reset()
+		w.buf.Write(data[n:])
+	}
+	return n, nil
+}
+
+func (w *wrapTunnelStream) Write(p []byte) (n int, err error) {
+	err = w.Send(&TunnelPackage{
+		Data: p,
+	})
+	if err != nil {
+		return
+	}
+
+	return len(p), nil
 }
--- service.go
+++ service.go
@@ -2,10 +2,18 @@ package fxxk
 
 import (
 	fmt "fmt"
+	"io"
+	"log"
+	"net"
 	"sync"
 
 	grpc "google.golang.org/grpc"
 )
+
+// ConnectionHandler 处理进来的连接
+type ConnectionHandler interface {
+	HandleAndListen(addr string)
+}
 
 type Service struct {
 	mu      *sync.Mutex
@@ -23,14 +31,45 @@ func (s *Service) Register(gs *grpc.Server) {
 	RegisterFxxkServer(gs, s)
 }
 
-func (s *Service) newTunelOfClientID(clientID string, tunel tunelStream) error {
+func (s *Service) getTunnel() (stream *tunnelStream) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for _, c := range s.clients {
+		if len(c.idleTunnels) > 0 {
+			stream = c.idleTunnels[len(c.idleTunnels)-1]
+			c.idleTunnels = c.idleTunnels[:len(c.idleTunnels)-1]
+			return
+		}
+	}
+	return
+}
+
+func (s *Service) acquireTunnel() (succeed bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for _, c := range s.clients {
+		if err := c.stream.Send(&Command{Type: Command_NEW_TUNEL}); err != nil {
+			log.Printf("send command to client %s error: %s", c.id, err)
+			// TODO close client
+			continue
+		}
+		succeed = true
+	}
+
+	if !succeed {
+		log.Println("no clients")
+	}
+	return
+}
+
+func (s *Service) newTunelOfClientID(clientID string, tunnel *tunnelStream) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	client, exists := s.clients[clientID]
 	if !exists {
 		return fmt.Errorf("client id %s not exists", clientID)
 	}
-	client.idleTunel = append(client.idleTunel, tunel)
+	client.idleTunnels = append(client.idleTunnels, tunnel)
 
 	s.newTunelSig.Signal()
 	return nil
@@ -66,7 +105,7 @@ func (s *Service) ProxyTunel(
 	if !ok {
 		return fmt.Errorf("need sent client id first")
 	}
-	ts := tunelStream{Fxxk_ProxyTunelServer: stream, done: make(chan struct{})}
+	ts := &tunnelStream{Fxxk_ProxyTunelServer: stream, done: make(chan struct{})}
 
 	if err := s.newTunelOfClientID(id.ClientId, ts); err != nil {
 		return err
@@ -74,4 +113,78 @@ func (s *Service) ProxyTunel(
 
 	<-ts.done
 	return nil
+}
+
+func (s *Service) HandleAndListen(addr string) {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		tcpConn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		log.Println("new connection from handle:", tcpConn.RemoteAddr())
+		go s.controls(tcpConn)
+	}
+}
+
+func (s *Service) waitAndGetTunnel() (stream *tunnelStream) {
+	for {
+		stream := s.getTunnel()
+		if stream == nil && !s.acquireTunnel() {
+			return nil
+		}
+		s.newTunelSig.Wait()
+	}
+}
+
+func (s *Service) controls(conn net.Conn) {
+	stream := s.waitAndGetTunnel()
+	if stream == nil {
+		conn.Close()
+		fmt.Println("cant get tunnel for conn:", conn.RemoteAddr())
+		return
+	}
+	done1 := make(chan struct{})
+	done2 := make(chan struct{})
+
+	rw := stream.ReadWriter()
+	s.atob(rw, conn, done1)
+	s.atob(conn, rw, done2)
+
+	select {
+	case <-done1:
+	case <-done2:
+	}
+	stream.Close()
+	conn.Close()
+}
+
+func (s *Service) atob(a io.Reader, b io.Writer, done chan struct{}) {
+	defer func() {
+		close(done)
+	}()
+
+	var buf [1024 * 4]byte
+	for {
+		var err error
+		n, err := a.Read(buf[:])
+		if err != nil {
+			fmt.Println("read conn error")
+			return
+		}
+
+		var writed int
+		for writed != n {
+			w, err := b.Write(buf[writed:n])
+			if err != nil {
+				fmt.Println("write conn error")
+				return
+			}
+			writed += w
+		}
+	}
 }
